a38905370318a76170fbb9a9c6aa51d02fb526c0
Sado Jeong <a01063854375@gmail.com>
2023-02-07 12:26:48 +0900

    FollowerModal

--- src/components/Modal/FollowerModal.jsx
+++ src/components/Modal/FollowerModal.jsx
@@ -3,13 +3,17 @@ import Modal from 'react-modal'
 
 const FollowerModal = () => {
     return (
-        <Modal className='outline-none flex fixed z-auto text-center -translate-x-1/2 -translate-y-1/2 bg-white border-2 w-[800px] rounded-2xl h-[650px] top-1/2 left-1/2'
-            isOpen={true} ariaHideApp={false}>
-            <div className='w-1/5 p-1 border-r-2 h-1/5'>
-                안녕하세요
-            </div>
+        <Modal id='FollowerModal'
+            style={{
+                overlay: {
+                    backgroundColor: 'rgba(255, 255, 255, 0.5)'
+                }
+            }}
+            isOpen={true} ariaHideApp={false}
+            className='outline-none absolute z-50 p-2 text-center -translate-x-1/2 -translate-y-1/2 bg-white border-2 w-[400px] rounded-2xl h-[650px] top-1/2 left-1/2'>
         </Modal>
-    )
+    );
 }
+
 
 export default FollowerModal
